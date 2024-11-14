a767668c2a6cb97e096bb6202d5e2f3af638fbc0
Diogo-Alves04 <diogo.carvalho.candeias@student.nhlstenden.com>
2024-11-14 14:38:44 +0100

    Added Rotate Left-Right

--- LosPerdidos_MotoMoto/src/main.cpp
+++ LosPerdidos_MotoMoto/src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 
-#define motorLSpeed 255
-#define motorRSpeed 255
+#define motorLFFullSpeed 240
+#define motorRFFullSpeed 254
+#define motorLBFullSpeed 243
+#define motorRBFullSpeed 255
+#define motorLHalfSpeed 177
+#define motorRHalfSpeed 177
 #define motorStop 0
 #define motorLB 5
 #define motorRB 10
@@ -25,36 +29,56 @@ void loop() {
   delay(1000);
   goLeft();
   delay(1000);
+  rotateLeft();
+  delay(1000);
+  rotateRight();
+  delay(1000);
 }
 
 void goForwards()
 {
-  analogWrite(motorLF, motorLSpeed);
-  analogWrite(motorRF, motorRSpeed);
+  analogWrite(motorLF, motorLFFullSpeed);
+  analogWrite(motorRF, motorRFFullSpeed);
   analogWrite(motorLB, motorStop);
   analogWrite(motorRB, motorStop);
 }
 
 void goBackwards()  
 { 
-  analogWrite(motorLB, motorLSpeed);
-  analogWrite(motorRB, motorRSpeed);
+  analogWrite(motorLB, motorLBFullSpeed);
+  analogWrite(motorRB, motorRBFullSpeed);
   analogWrite(motorLF, motorStop);
   analogWrite(motorRF, motorStop);
 }
 
 void goRight()
 {
-  analogWrite(motorLF, motorLSpeed);
-  analogWrite(motorRF, motorStop);
+  analogWrite(motorLF, motorLFFullSpeed);
+  analogWrite(motorRF, motorRHalfSpeed);
   analogWrite(motorLB, motorStop);
   analogWrite(motorRB, motorStop);
 }
 
 void goLeft()
 {
-  analogWrite(motorLB, motorStop);
-  analogWrite(motorRB, motorRSpeed);
+  analogWrite(motorLB, motorLHalfSpeed);
+  analogWrite(motorRB, motorRFFullSpeed);
   analogWrite(motorLF, motorStop);
   analogWrite(motorRF, motorStop);
+}
+
+void rotateRight()
+{
+  analogWrite(motorLB, motorLBFullSpeed);
+  analogWrite(motorRB, motorStop);
+  analogWrite(motorRF, motorRFFullSpeed);
+  analogWrite(motorLF, motorStop);
+}
+
+void rotateLeft()
+{
+  analogWrite(motorRB, motorRBFullSpeed);
+  analogWrite(motorLB, motorStop);
+  analogWrite(motorLF, motorLFFullSpeed);
+  analogWrite(motorLF, motorStop);
 }
